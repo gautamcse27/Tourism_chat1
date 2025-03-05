3ca482c0879876fa894a32981eda47fbf5b89659
gautamcse27 <khushi27gkcse@gmail.com>
2025-03-05 11:32:26 +0530

    Update TouristPlace.kt

--- app/src/main/java/com/example/tourismchat1/data/models/TouristPlace.kt
+++ app/src/main/java/com/example/tourismchat1/data/models/TouristPlace.kt
@@ -4,6 +4,8 @@ import com.google.gson.annotations.SerializedName
 data class TouristPlace(
     @SerializedName("id")val id: Int,
     @SerializedName("name") val name: String,
+    @SerializedName("latitude") val latitude: Double,  // ✅ Added latitude
+    @SerializedName("longitude") val longitude: Double,
     @SerializedName("location") val location: String,
     @SerializedName("description") val description: String,
     @SerializedName("image_urls") val image_urls: List<String>
